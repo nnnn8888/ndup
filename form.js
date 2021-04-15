0bb8999c539d77bf839ddccce16e3507c468c82f
nnnn8888 <80675401+nnnn8888@users.noreply.github.com>
2021-04-15 08:34:59 +0200

    Add files via upload

--- form.js
+++ form.js
@@ -1,53 +1,46 @@
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
+let form = document.querySelector('#contact')
+let button = form.querySelector('button[type=submit]')
+let buttonText = button.textContent
+
+form.addEventListener('submit', async function (e) {
+  button.disabled = true
+  button.textContent = 'Chargement...'
+  e.preventDefault()
+  let data = new FormData(form)
+  try {
+    let response = await fetch(form.getAttribute('action'), {
+      method: 'POST',
+      headers: {
+        'X-Requested-With': 'xmlhttprequest'
+      },
+      body: data
+    })
+    let responseData = await response.json()
+    // La réponse n'est pas bonne (pas 200), on affiche les erreurs
+    if (response.ok === false) {
+      let errors = responseData
+      let errorsKey = Object.keys(errors)
+      for (let i = 0; i < errorsKey.length; i++) {
+        let key = errorsKey[i]
+        let error = errors[key]
+        let input = document.querySelector('[name=' + key + ']')
+        let span = document.createElement('span')
+        span.className = 'help-block'
+        span.innerHTML = error
+        input.parentNode.appendChild(span)
+      }
+    // La réponse est ok, on vide le formulaire
     } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
+      let inputs = form.querySelectorAll('input, textarea')
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ''
+      }
+      alert('Merci pour votre message')
     }
-});
-
-
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
-    });
-}
-
-function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
+  } catch (e) {
+    alert(e)
+  }
+  // Dans tous les cas on permet la soumission du formulaire à nouveau
+  button.disabled = false
+  button.textContent = buttonText
+})
